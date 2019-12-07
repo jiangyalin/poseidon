d8b5a10c86cef9c036aab73f358ef9303ef13435
yalin.jiang <yalin.jiang@71360.com>
2019-12-07 13:49:20 +0800

    初始化

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,6 @@ module.exports = function (app) {
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json()); // 解析参数
 
-  // 登陆拦截
-  app.use(function (req, res, next) {
-    if (!req.session.UrlReferrer) {
-      req.session.UrlReferrer = '/index'; // 记录用户的访问地址
-    }
-    if (req.originalUrl.indexOf('/login') === -1 && req.originalUrl.indexOf('.') === -1) {
-      req.session.UrlReferrer = req.originalUrl; // 记录用户的访问地址
-      if (!req.session.user) {
-        return res.redirect('/login');
-      }
-    }
-    next();
-  });
-
   app.get('/', function (req, res) {
     return res.redirect('/index');
   });
